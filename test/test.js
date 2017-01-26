22676f452f915e0f6bcc285c1dae19454c2ca4c4
mertkahyaoglu <mertkahyaoglu93@gmail.com>
2017-01-26 14:31:40 +0200

    update

--- test/test.js
+++ test/test.js
@@ -3,9 +3,8 @@ var assert = require('assert');
 var counter = require('../');
 
 describe('array-counter node module', function () {
-  it('must have at least one test', function () {
+  it('must count correctly', function () {
     assert.equal(counter(["test", 1, 2, "test", "counter", [1,2,3]])['test'], 2);
-    var arr = ["foo", "bar", 10, 40, "foo", 10, "Hello World"];
-    console.log(counter(arr));
+    assert.equal(counter([1, 1, 2, {a:'b', c:'c'}])['1'], 2);
   });
 });
